a5010425a3864e17a4f54883f9d5de3eba8ac88e
jeehyekim <iam@jeehyekim.com>
2015-10-10 18:06:35 -0700

    project commit

--- base.js
+++ base.js
@@ -2,6 +2,18 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  // code in here
+	if ($('#micro-content').val().trim().length > 0) {
+		var microContent = $('#micro-content').val();
+		var newContent = '<li class="list-group-item">' + microContent + '</li>';
+		$('#micro-posts').prepend(newContent);
+		$('#todo-content').val('');
+
+		console.log($('#todos').children().last());
+		// click on list-item 
+		// $('#todos').children().last().on('click', function() {
+		//   strikeThrough($(this));
+		// });
+
+	}
 
 });
