38ee89ef166734e77b22b1311656d77f69a15fb2
jeehyekim <iam@jeehyekim.com>
2015-10-11 21:15:50 -0700

    project commit

--- base.js
+++ base.js
@@ -1,19 +1,38 @@
-console.log("Sanity Check: JS is working!");
+
 
 $(document).ready(function(){
+	console.log("Sanity Check: JS is working!");
+			
+			var count = 0;
+			var date = new Date();
+			var hours = date.getHours();
+			var minutes = date.getMinutes();
+			var postTime = hours + ":" + minutes;
+			var month = date.getMonth();
+			var day = date.getDate();
+			var year = date.getFullYear();
+			var postDate =  (month+1) + "/" + day + "/" + year;
+			var heartIcon = "<i class='ion-heart' role='button'>" + count + "</i>";
 
-	if ($('#micro-content').val().trim().length > 0) {
-		var microContent = $('#micro-content').val();
-		var newContent = '<li class="list-group-item">' + microContent + '</li>';
-		$('#micro-posts').prepend(newContent);
-		$('#todo-content').val('');
 
-		console.log($('#todos').children().last());
-		// click on list-item 
-		// $('#todos').children().last().on('click', function() {
-		//   strikeThrough($(this));
-		// });
 
-	}
+
+	$('#micro-content').keydown(function(event) {
+		if(event.keyCode == 13) {
+			var count = 0;
+			if ($('#micro-content').val().trim().length > 0) {
+				var microContent = $('#micro-content').val();
+				var newContent = "<div class='list-group-item'>" + microContent + "<br><span id='date'></span></div>";
+				$('#micro-posts').prepend(newContent).each(function(){$('#date',this).html(postDate + "      " + postTime + heartIcon);});
+				$('#micro-content').val('');
+		$('.list-group-item').on('click',function(e) {
+			$('#date').children().html(count++);
+			// console.log(count++);
+		});
+		
+			}
+		}
+
+	});
 
 });
